37024e0ee26f2f10062a0defb8c0831a4db174cb
Ali Hadi Mazeh <alihadimazeh@cmail.carleton.ca>
2023-04-16 03:50:50 -0400

    in Session class: finished calculateHRV(), provided a template for coherence score function

--- FinalProject/Session.cpp
+++ FinalProject/Session.cpp
@@ -33,7 +33,7 @@ Session::~Session() {
 
 /* Function that calculates the current heart rate variability (HRV)
  */
-float Session::calculateHRV() {
+double Session::calculateHRV(QVector<double>& heartRates) {
     //how does one math?? ;-;
     //TODO: figure out math and get gud
     //Helpful resource?? -> https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5624990/#:~:text=It%20is%20calculated%20by%20first,average%20of%20these%20288%20values.
@@ -41,15 +41,51 @@ float Session::calculateHRV() {
     // loop between all of the heartbeats
     // calculate HRV point between every heartbeat
     // append that calculated HRV to the graph
-    return 0.0f; //PLACEHOLDER! CHANGE LATER
+    // HRV is a measure of the variation in time intervals between heartbeats.
+
+    QVector<double> rrIntervals;
+    for (int i=1; i<heartRates.size(); i++){
+        double rrInterval = heartRates[i] - heartRates[i-1];
+        rrIntervals.append(rrInterval);
+    }
+
+    // calculate the mean RR interval
+    double meanRRInterval = 0.0;
+    for (int i = 0; i < rrIntervals.size(); i++) {
+        meanRRInterval += rrIntervals[i];
+    }
+    meanRRInterval /= rrIntervals.size();
+
+    // calculate the standard deviation of RR intervals
+    double variance = 0.0;
+    for (int i = 0; i < rrIntervals.size(); i++) {
+        variance += pow(rrIntervals[i] - meanRRInterval, 2);
+    }
+    variance /= rrIntervals.size() - 1;
+    double stdDev = sqrt(variance);
+
+    // calculate HRV in milliseconds
+    double hrv = stdDev * 1000;
+
+    return hrv;
 }
 
 /* Function that calculates a coherence score acording to the current state
  * DEVELOPER NOTE: not sure if we need this if we are using a set of predefined score, function
  *                 might be removed in a future commit
  */
-float Session::calculateCoherence(void) {
-    //send help T-T
+double Session::calculateCoherence(int flag) {
+    // flag determines which coherence level
+    // low coherence
+    if (flag == 0){
+        // generate low coherence data
+    }
+    else if (flag == 1){
+        // generate medium coherence level
+    }
+    else if (flag == 2){
+        // generate high coherence level
+    }
 
     return 0.0f; //PLACEHOLDER! CHANGE LATER
 }
@@ -57,7 +93,7 @@ float Session::calculateCoherence(void) {
 /* Function that calulates the optimal breathPacer setting and returns it
  * Returns: optimal breathPacer setting
  */
-float Session::updateBreathPacer(void) {
+double Session::updateBreathPacer(void) {
     //I have no clue
 
     return 0.0f; //PLACEHOLDER! CHANGE LATER
